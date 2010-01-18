5d7bf74c3cf352667012bc2daacb180d484c5c6e
Chris Forbes <chrisf@ijw.co.nz>
2010-01-18 13:58:20 +1300

    initial commit

---
+++ metrics.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Simple code metrics extractor for C# (and similar languages)
+# usage: metrics PATTERN
+# example: metrics *.cs
+
+echo -n 'Total Lines: '
+find . -iname $1 | xargs cat | wc -l
+echo -n 'Non-Blanks:  '
+find . -iname $1 | xargs grep -ve '^\s*$' | wc -l
+echo -n '// Comments: '
+find . -iname $1 | xargs grep -e '//' | wc -l
+echo -n '/* Comments: '
+find . -iname $1 | xargs grep -e '/\*' | wc -l
+echo -n 'Semicolons:  '
+find . -iname $1 | xargs grep ';' | wc -l
+echo -n 'Classes:     '
+find . -iname $1 | xargs grep 'class' | wc -l
+echo -n 'Interfaces:  '
+find . -iname $1 | xargs grep 'interface' | wc -l
+echo -n 'Namespaces:  '
+find . -iname $1 | xargs grep -h 'namespace' | sort | uniq | wc -l
+echo -n 'Files:       '
+find . -iname $1 | wc -l
